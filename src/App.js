676b0d19d0e85717d5648f196df6f7e2d77d51f6
Jim Raptis <dimitrisraptis96@gmail.com>
2020-06-24 10:51:42 +0300

    Fix Editable component styles

--- src/App.js
+++ src/App.js
@@ -1,76 +1,91 @@
 import React from "react";
 import TemplateIcon from "./TemplateIcon";
 import SaveIcon from "./SaveIcon";
-import { ThemeProvider } from "@chakra-ui/core";
-import { Editable, EditableInput, EditablePreview } from "@chakra-ui/core";
-import { CSSReset, Button, Text, Stack, Tooltip } from "@chakra-ui/core";
-import "./styles.css";
+import {
+  Editable,
+  EditableInput,
+  EditablePreview,
+  Button,
+  Text,
+  Stack,
+  Tooltip
+} from "@chakra-ui/core";
 
 export default function App() {
+  const [name, setName] = React.useState("Personal Blog post template");
+
   return (
-    <ThemeProvider>
-      <div
-        className="App"
+    <div
+      className="App"
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100vw",
+        height: "100vh",
+        backgroundColor: "#F3F2F2"
+      }}
+    >
+      <Stack
+        direction="row"
+        justify="space-between"
+        align="center"
         style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          width: "100vw",
-          height: "100vh",
-          backgroundColor: "#F3F2F2"
+          // display: "block",
+          width: "80%",
+          backgroundColor: "white",
+          boxShadow:
+            "0px 0px 1px rgba(0, 0, 0, 0.04), 0px 2px 6px rgba(0, 0, 0, 0.04), 0px 10px 20px rgba(0, 0, 0, 0.04)",
+          borderRadius: "16px",
+          padding: "32px"
         }}
       >
-        <CSSReset />
-
-        <Stack
-          direction="row"
-          justify="space-between"
-          align="center"
-          style={{
-            // display: "block",
-            width: "80%",
-            backgroundColor: "white",
-            boxShadow:
-              "0px 0px 1px rgba(0, 0, 0, 0.04), 0px 2px 6px rgba(0, 0, 0, 0.04), 0px 10px 20px rgba(0, 0, 0, 0.04)",
-            borderRadius: "16px",
-            padding: "32px"
-          }}
-        >
-          <Stack direction="row" spacing={16} isInline>
-            <TemplateIcon />
-            <Stack spacing={0}>
-              <Text margin={0} fontSize={12} color="#727272">
-                Template
-              </Text>
-              <Tooltip label="Click to edit the name">
-                <Editable
-                  margin="0"
-                  fontSize={18}
-                  defaultValue="Personal Blog post template"
-                >
-                  <EditablePreview />
-                  <EditableInput />
-                </Editable>
-              </Tooltip>
-            </Stack>
+        <Stack direction="row" spacing={16}>
+          <TemplateIcon />
+          <Stack spacing={0}>
+            <Text margin={0} fontSize={12} color="#727272">
+              Template
+            </Text>
+            <Tooltip
+              label="Click to edit the name"
+              bg="black"
+              borderRadius="4px"
+              color="white"
+              padding="0.1rem 0.4rem"
+              placement="bottom"
+              fontSize="14px"
+            >
+              <Editable
+                cursor="pointer"
+                margin="0"
+                fontSize={18}
+                defaultValue={name}
+                onSubmit={setName}
+              >
+                <EditablePreview _hover={{ color: "#727272" }} />
+                <EditableInput width="250px" />
+              </Editable>
+            </Tooltip>
           </Stack>
-
-          <Button
-            border="2px"
-            variant="solid"
-            leftIcon={<SaveIcon />}
-            shadow="0px 0px 1px rgba(0, 0, 0, 0.04), 0px 2px 6px rgba(0, 0, 0, 0.08), 0px 16px 24px rgba(0, 0, 0, 0.1), 0px 24px 32px rgba(0, 0, 0, 0.14)"
-            width="150px"
-            borderRadius="8px"
-            bgColor="black"
-            color="white"
-            height="43px"
-            fontSize="16px"
-          >
-            Save
-          </Button>
         </Stack>
-      </div>
-    </ThemeProvider>
+
+        <Button
+          // border="2px"
+          variant="solid"
+          leftIcon={<SaveIcon />}
+          boxShadow="0px 0px 1px rgba(0, 0, 0, 0.04), 0px 2px 6px rgba(0, 0, 0, 0.08), 0px 16px 24px rgba(0, 0, 0, 0.1), 0px 24px 32px rgba(0, 0, 0, 0.14)"
+          width="150px"
+          borderRadius="4px"
+          bg="black"
+          color="white"
+          _hover={{ transform: "translateY(-1px)", boxShadow: "none" }}
+          height="43px"
+          fontSize="16px"
+          fontWeight="500"
+        >
+          Save
+        </Button>
+      </Stack>
+    </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { ThemeProvider, CSSReset } from "@chakra-ui/core";
 import App from "./App";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <React.StrictMode>
+  <ThemeProvider>
+    <CSSReset />
     <App />
-  </React.StrictMode>,
+  </ThemeProvider>,
   rootElement
 );
